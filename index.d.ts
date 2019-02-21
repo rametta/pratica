ddb7606eb9eb9adc4dd8c69dfdadfd211c0dbaa6
Jason Rametta <jrametta@dynamite.ca>
2019-02-21 08:29:09 -0500

    types

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,33 @@
-declare module pratica {
+type Maybe<T> = Just<T>|Nothing
 
-  export type Maybe = <T>(arg: T) => Just<T>|Nothing
-
-  export type Just = <T>(arg: T) => ({
-    ap(cb: () => any): Maybe<T>;
-    map(cb: () => any): Just<T>;
-    chain(cb: () => any): T;
-    default(cb: () => any): Maybe<T>;
-    cata: {
-      Just(arg: T): any,
-      Nothing(): any
-    };
-    inspect(): string;
-    isNothing(): boolean;
-    isJust(): boolean;
-  })
-
-  export type Nothing = () => ({
-    ap(cb: () => any): Nothing;
-    map(cb: () => any): Nothing;
-    chain(cb: () => any): Nothing;
-    default(cb: () => any): Maybe;
-    cata: {
-      Just(arg): any,
-      Nothing(): any
-    };
-    inspect(): string;
-    isNothing(): boolean;
-    isJust(): boolean;
-  })
-  
+type Just<T> = {
+  ap(cb: () => T): Maybe<T>
+  map(cb: (arg: T) => T): Just<T>
+  chain(cb: () => Maybe<T>): T
+  default(cb: () => T): Maybe<T>
+  cata: {
+    Just(arg: T): any
+    Nothing(): any
+  }
+  inspect(): string
+  isNothing(): boolean
+  isJust(): boolean
 }
+
+type Nothing = {
+  ap(cb: () => any): Nothing
+  map(cb: () => any): Nothing
+  chain(cb: () => any): Nothing
+  default(cb: () => any): Maybe<any>
+  cata: {
+    Just(arg): any
+    Nothing(): any
+  }
+  inspect(): string
+  isNothing(): boolean
+  isJust(): boolean
+}
+
+export function Maybe<T>(arg: T): Just<T> | Nothing
+export function Just<T>(arg: T): Just<T>
+export function Nothing(): Nothing
